2efd047be94c3dde5f72cfabffc4549a2eb20348
dogyhbin2 <dogyhbin12345@gmail.com>
2018-08-07 20:48:50 +0900

    Splash Activity Finished

---
+++ app/src/main/java/com/threehundred/ahwa_android/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.threehundred.ahwa_android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private final int DELAY_TIME = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, DELAY_TIME);
+    }
+}
